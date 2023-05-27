7142e910a40fb049b624fa76ab31b02a474b7c6f
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2023-05-27 22:40:06 +0900

    Add bitmap draw fun

--- app/src/main/java/com/github/masato1230/bitmapcomvertible/MainActivity.kt
+++ app/src/main/java/com/github/masato1230/bitmapcomvertible/MainActivity.kt
@@ -7,26 +7,44 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.view.drawToBitmap
 import com.github.masato1230.bitmapcomvertible.ui.theme.BitmapComvertibleTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val coroutineScope = rememberCoroutineScope()
             BitmapComvertibleTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.train),
-                        contentDescription = "train",
+                    AndroidView(
+                        factory = {
+                            val view = ComposeView(it).apply {
+                                setContent {
+                                    Image(
+                                        painter = painterResource(id = R.drawable.train),
+                                        contentDescription = "train",
+                                    )
+                                }
+                            }
+                            coroutineScope.launch {
+                                delay(1000)
+                                val bitmap = view.drawToBitmap()
+                                bitmap
+                            }
+                            return@AndroidView view
+                        },
                     )
                 }
             }
