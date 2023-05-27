dc697daf36f0eca5cd567e67989daeee2819a010
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2023-05-27 22:54:57 +0900

    Add state class

--- app/src/main/java/com/github/masato1230/bitmapcomvertible/MainActivity.kt
+++ app/src/main/java/com/github/masato1230/bitmapcomvertible/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.github.masato1230.bitmapcomvertible
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,7 +8,10 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.painterResource
@@ -15,39 +19,62 @@ import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.view.drawToBitmap
 import com.github.masato1230.bitmapcomvertible.ui.theme.BitmapComvertibleTheme
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val coroutineScope = rememberCoroutineScope()
             BitmapComvertibleTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AndroidView(
-                        factory = {
-                            val view = ComposeView(it).apply {
-                                setContent {
-                                    Image(
-                                        painter = painterResource(id = R.drawable.train),
-                                        contentDescription = "train",
-                                    )
-                                }
-                            }
-                            coroutineScope.launch {
-                                delay(1000)
-                                val bitmap = view.drawToBitmap()
-                                bitmap
-                            }
-                            return@AndroidView view
-                        },
-                    )
+                    val bitmapConvertibleState = rememberBitmapConvertibleState()
+                    BitmapConvertible(state = bitmapConvertibleState) {
+                        Image(
+                            painter = painterResource(id = R.drawable.train),
+                            contentDescription = "train",
+                        )
+                    }
+
+                    LaunchedEffect(key1 = Unit) {
+                        delay(1000)
+                        val bitmap = bitmapConvertibleState.getBitmap();
+                    }
                 }
             }
         }
     }
 }
+
+@Composable
+fun BitmapConvertible(
+    state: BitmapConvertibleState,
+    content: @Composable () -> Unit,
+) {
+    AndroidView(
+        factory = {
+            val view = ComposeView(it).apply {
+                setContent(content)
+            }
+            state.setComposeView(view)
+            return@AndroidView view
+        },
+    )
+}
+
+@Stable
+class BitmapConvertibleState {
+    private lateinit var _composeView: ComposeView
+    fun setComposeView(view: ComposeView) {
+        _composeView = view
+    }
+
+    fun getBitmap(config: Bitmap.Config = Bitmap.Config.ARGB_8888): Bitmap {
+        return _composeView.drawToBitmap(config = config)
+    }
+}
+
+@Composable
+fun rememberBitmapConvertibleState() = remember { BitmapConvertibleState() }
