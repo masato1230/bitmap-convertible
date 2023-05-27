ef2af4681e642b6597af0cd6ab3b862c5752bc1a
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2023-05-27 23:20:52 +0900

    Show bitmap as a thumbnail

--- app/src/main/java/com/github/masato1230/bitmapcomvertible/MainActivity.kt
+++ app/src/main/java/com/github/masato1230/bitmapcomvertible/MainActivity.kt
@@ -4,17 +4,29 @@ import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.view.drawToBitmap
 import com.github.masato1230.bitmapcomvertible.ui.theme.BitmapComvertibleTheme
@@ -25,22 +37,48 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BitmapComvertibleTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    val bitmap = remember { mutableStateOf<Bitmap?>(null) }
                     val bitmapConvertibleState = rememberBitmapConvertibleState()
-                    BitmapConvertible(state = bitmapConvertibleState) {
-                        Image(
-                            painter = painterResource(id = R.drawable.train),
-                            contentDescription = "train",
-                        )
-                    }
-
-                    LaunchedEffect(key1 = Unit) {
-                        delay(1000)
-                        val bitmap = bitmapConvertibleState.getBitmap();
+                    Box {
+                        BitmapConvertible(state = bitmapConvertibleState) {
+                            Image(
+                                painter = painterResource(id = R.drawable.train),
+                                contentDescription = "train",
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .pointerInput(Unit) {
+                                        detectTapGestures(
+                                            onLongPress = {
+                                                bitmap.value = bitmapConvertibleState.getBitmap()
+                                            },
+                                        )
+                                    },
+                            )
+                        }
+                        bitmap.value?.let {
+                            var isShowThumbnail by remember { mutableStateOf(false) }
+                            LaunchedEffect(key1 = bitmap.value) {
+                                isShowThumbnail = true
+                                delay(5000)
+                                isShowThumbnail = false
+                            }
+                            AnimatedVisibility(
+                                visible = isShowThumbnail,
+                                modifier = Modifier.align(Alignment.BottomEnd)
+                            ) {
+                                Image(
+                                    bitmap = it.asImageBitmap(),
+                                    contentDescription = "thumbnail",
+                                    modifier = Modifier
+                                        .width(100.dp)
+                                        .padding(20.dp),
+                                )
+                            }
+                        }
                     }
                 }
             }
@@ -51,9 +89,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun BitmapConvertible(
     state: BitmapConvertibleState,
+    modifier: Modifier = Modifier,
     content: @Composable () -> Unit,
 ) {
     AndroidView(
+        modifier = modifier,
         factory = {
             val view = ComposeView(it).apply {
                 setContent(content)
